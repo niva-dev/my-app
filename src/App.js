c134a6b40d879536b8d0d454e2e3008519ff86eb
niva-dev <52524674+niva-dev@users.noreply.github.com>
2020-02-12 18:18:25 +0300

    dev component functions

--- src/App.js
+++ src/App.js
@@ -16,70 +16,65 @@ const App = () => (
 const Boot_Form = (props) => {
 
 
-//let input_email = '';
-//let formValid = false;
-let input_password = '';
-let emailValid = false;
-let passwordValid = false;
+  //let input_email = '';
+  //let formValid = false;  
+  //let input_password = '';
+  let emailValid = false;
+  let passwordValid = false;
 
-const [input_email, setEmail] = useState('');
+  const [input_email, setEmail] = useState('');
 
-const [formValid, setFormState] = useState(false);
+  const [input_password, setPass] = useState('');
 
+  const [formValid, setFormState] = useState(false);
   
-const validateField = (fieldName, value) => {
-    switch (fieldName) {
-      case 'input_email':
-        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-       // setEmail(value);
-        break;
-      case 'input_password':
-        passwordValid = value.length >= 6;
-        break;
-      default:
-        break;
-    }
+
+  const validateForm = () => {
+    console.log("ev: " + emailValid + " pv: " + passwordValid);
+    setFormState(emailValid && passwordValid);
   }
 
- const validateForm = () => {
-  console.log("ev: " + emailValid + " pv: " + passwordValid);
-    setFormState(emailValid && passwordValid);    
+  const handleEmailInput = (event) => {
+    setEmail(event.target.value);
+    emailValid = event.target.value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    validateForm();
   }
 
-  const handleUserInput = (event) => {     
-    validateField(event.target.name, event.target.value);    
-    console.log("n: " + emailValid);
+  const handlePassInput = (event) => {
+    setPass(event.target.value);
+    passwordValid = event.target.value.length >= 6;
     validateForm();
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    document.getElementsByClassName("form")[0].hidden = true;  
-    document.getElementsByName("welcome")[0].hidden = false;  
+    document.getElementsByClassName("form")[0].hidden = true;
+    document.getElementsByName("welcome")[0].hidden = false;
   }
 
-  
-  return ([
-    
-    <h3 name="welcome" hidden>Привет, {input_email} </h3>,
-      <Form onSubmit={handleSubmit} className="form">
+
+  return (
+    <React.Fragment>
+      <h3 name="welcome" hidden>Привет, {input_email} </h3>
+      <form onSubmit={handleSubmit} className="form">
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" name="input_email" onChange={handleUserInput} />
+          <Form.Control type="email" placeholder="Enter email" name="input_email" onChange={handleEmailInput} value={input_email} />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" name="input_password" onChange={handleUserInput} />
+          <Form.Control type="password" placeholder="Password" name="input_password" onChange={handlePassInput} value={input_password}/>
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
         <Button variant="primary" type="submit" name="but_submit" disabled={!formValid} >
           Submit
-        </Button>
-      </Form> 
-  ]);
+          </Button>
+      </form>
+    </React.Fragment>
+  );
 }
 
 export default App;
